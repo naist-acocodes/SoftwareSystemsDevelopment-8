f2a4953916fa7bd55540e40b3548347be6609bbe
naist-acocodes <elyanah_marie_cariaga.aco.ef1@naist.ac.jp>
2025-01-06 13:05:24 +0900

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -51,7 +51,7 @@ function TodoListCard() {
 
     return (
         <React.Fragment>
-            <h1>This is Elyanah's todo list!</h1>
+            <h1>This is naist-acocodes' todo list!</h1>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
                 <p className="text-center">No items yet! Add one above!</p>
